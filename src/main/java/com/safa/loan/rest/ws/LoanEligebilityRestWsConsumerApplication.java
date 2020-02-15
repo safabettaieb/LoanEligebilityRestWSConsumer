129a12b4ad25d82889083562d28fcb09c6be0eb1
safa bettieb <safa.bettaieb1@gmail.com>
2020-02-15 22:53:42 +0100

    first commit

---
+++ src/main/java/com/safa/loan/rest/ws/LoanEligebilityRestWsConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.safa.loan.rest.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanEligebilityRestWsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanEligebilityRestWsConsumerApplication.class, args);
+	}
+
+}
